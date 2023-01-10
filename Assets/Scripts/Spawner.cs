76e9bb321cee6579905fbf23b8aa77455f185ba8
Robin-Penot <rpenot86@gmail.com>
2023-01-10 09:49:41 +0100

    Cubes Spawnes and start to move in the direction of the camera

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,23 +7,28 @@ public class Spawner : MonoBehaviour
     [SerializeField] private GameObject Object;
     [SerializeField] private float radius =5f;
     [SerializeField] private float SpawnSpeed =3f;
-
     private float time = 0f;
+    [SerializeField] private float circleSpeed = 1f;
 
+void Start()
+{
 
+}
     void Update()
     {
         if(SpawnSpeed <= 0f) return;
 
         time += Time.deltaTime;
 
-            GameObject newObject;
+        GameObject newObject;
+       // gameObject.transform.position += Camera.main.transform.forward;
             if (time >= 1f/SpawnSpeed)
             {
                 Vector3 localPosition = Random.insideUnitCircle * radius;
                 Vector3 worldPosition = transform.TransformPoint(localPosition);
                 newObject = Instantiate(Object, worldPosition, Quaternion.identity);
                 newObject.GetComponent<Renderer>().material.color = Random.ColorHSV();
+                newObject.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * circleSpeed);
                 time = 0f;
             }
     }
